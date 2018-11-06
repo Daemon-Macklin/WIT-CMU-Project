ba4c0ca175b3a79dc1b13e033499b9a3b833ae03
andyAndyA <aw087ok@gmail.com>
2018-11-06 15:52:38 +0000

    changed blurring and binarization algorithms in pre processing

--- pytesseract-test.py
+++ pytesseract-test.py
@@ -1,15 +1,17 @@
 import cv2
 import sys
 import pytesseract
+import numpy as np
 
 def pre_process(image):
+
 	# https://medium.freecodecamp.org/getting-started-with-tesseract-part-i-2a6a6b1cf75e
 
 	# 0. Greyscaling the image
 	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 	# 1. Rescaling the image
-	image = cv2.resize(image, None, fx = 1.5, fy = 1.5)
+	image = cv2.resize(image, None, fx = 2, fy = 2, interpolation = cv2.INTER_CUBIC)
 
 	# 2. Apply dilation
 	kernel = np.ones((1, 1), np.uint8)
@@ -19,22 +21,23 @@ def pre_process(image):
 	image = cv2.erode(image, kernel, iterations = 1)
 
 	# 4. Apply gaussian blur to smooth out edges
-	image = cv2.GaussianBlur(image, (5, 5), 0)
+	# image = cv2.GaussianBlur(image, (5, 5), 0)
+	image = cv2.bilateralFilter(image, 9, 75, 75)
 
 	# 5. Converting image to black and white
 	# cv2.threshold returns (threshold, binary image) dict.
-	image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+	# image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+	image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
 
 	return image
 
 def main():
 
-	# Program must require a command line argument
+	# Getting the image path as a command line argument or input
 	if len(sys.argv) < 2:
-		print('Usage: python pytesseract-test.py [image-file]')
+		print("Usage: python2 pytesseract-test.py [image]")
 		sys.exit(1)
 
-	# Image path
 	imgPath = sys.argv[1]
 
 	# Reading in the image and performing noise removal
